5a73f4cb56298388c667c8a1b7485ac45e1e4542
Soedar <soedar.sg@gmail.com>
2013-05-04 13:40:36 -0700

    Added timer code

--- PlaybulbAH-iOS/PlaybulbAH/GameViewController.m
+++ PlaybulbAH-iOS/PlaybulbAH/GameViewController.m
@@ -8,9 +8,13 @@
 
 #import "GameViewController.h"
 
+const CGFloat DEFAULT_TICK_DURATION = 1;
+
 @interface GameViewController ()
 
 @property (nonatomic, strong) IBOutletCollection(UIButton) NSArray *buttonList;
+@property (nonatomic) CGFloat tickDuration;
+@property (nonatomic, strong) NSTimer *timer;
 
 @end
 
@@ -21,6 +25,7 @@
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
         // Custom initialization
+        self.tickDuration = DEFAULT_TICK_DURATION;
     }
     return self;
 }
@@ -29,6 +34,8 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    
+    self.timer = [NSTimer scheduledTimerWithTimeInterval:self.tickDuration target:self selector:@selector(tick:) userInfo:nil repeats:NO];
 }
 
 - (void)didReceiveMemoryWarning
@@ -37,6 +44,15 @@
     // Dispose of any resources that can be recreated.
 }
 
+#pragma mark - Ticks
+
+- (void) tick:(NSTimer*)timer
+{
+    
+    self.timer = [NSTimer scheduledTimerWithTimeInterval:self.tickDuration target:self selector:@selector(tick:) userInfo:nil repeats:NO];
+}
+
+#pragma mark - Button actions
 
 - (IBAction)buttonTapped:(UIButton*)button
 {
